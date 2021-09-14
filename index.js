dad6516593e4920cc908886cde7ad75b3492fbf4
RolandViper <rolandviper@gmail.com>
2021-09-14 17:30:03 +0800

    3.7-3.8

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+//Morgan
+morgan.token("person", (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+  return null;
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 app.use(express.json());
 
